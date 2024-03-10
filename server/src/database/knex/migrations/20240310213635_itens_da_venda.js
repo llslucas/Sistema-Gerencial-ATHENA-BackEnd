a6919970230b1dfd7722f24a05dfdad5a2f32211
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-03-10 18:50:20 -0300

    Migrations atualizadas

---
+++ server/src/database/knex/migrations/20240310213635_itens_da_venda.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    knex.schema.al
+    return knex.schema.createTable("itens_da_venda", table => {
+        table.increments("id");
+        table.integer("id_produto").references("id").inTable("produtos");
+        table.integer("id_venda").references("id").inTable("vendas").onDelete("CASCADE");
+        table.integer("quantidade");
+        table.float("valor_unitario");
+        table.float("valor_total");
+        table.float("valor_comissao");        
+
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable("itens_da_venda");
+};
---
+++ server/src/database/knex/migrations/20240310214155_itens_da_compra.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable("itens_da_compra", table => {
+        table.increments("id");
+        table.integer("id_produto").references("id").inTable("produtos");
+        table.integer("id_compra").references("id").inTable("compras").onDelete("CASCADE");
+        table.integer("quantidade");
+        table.float("valor_unitario");
+        table.float("valor_total");        
+
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable("itens_da_compra");
+};

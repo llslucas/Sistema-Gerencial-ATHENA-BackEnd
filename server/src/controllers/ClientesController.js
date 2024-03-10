24d4a85f9f618dde0d3fd9d4831dbbafd053bf2e
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-03-10 15:21:31 -0300

    Controller Clientes criado.

---
+++ server/src/controllers/ClientesController.js
@@ -0,0 +1,46 @@
+import knex from "../database/knex/index.js";
+
+export default class ClientesController{
+    async create(request,response){
+        const { nome, telefone, email } = request.body;
+
+        const [cliente_id] = await knex("clientes").insert({
+            nome,
+            telefone,
+            email
+        });
+
+        response.status(201).json("Novo cliente cadastrado com sucesso: " + cliente_id);
+    }
+
+    async show(request, response){
+        const { id } = request.params;
+
+        const cliente = await knex("clientes").where({ id }).first();
+
+        return response.json({
+            cliente
+        });
+    }
+
+    async delete(request, response){
+        const { id } = request.params;
+
+        await knex("clientes").where({ id }).delete();
+
+        return response.json();
+    }
+
+    async index(request, response){
+        const { nome } = request.query; 
+        const search = nome ?? '';        
+
+        const clientes = await knex("clientes")
+            .whereLike("clientes.nome", `%${search}%`)
+            .orderBy("nome");
+
+        return response.json(clientes);            
+    }
+
+    
+}

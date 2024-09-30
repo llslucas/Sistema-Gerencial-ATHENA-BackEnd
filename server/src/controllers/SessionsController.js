35814b7c8428a8a355bdd619b508ba0a5912f9e8
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-09-29 23:29:54 -0300

    Controller de Sessões criado

---
+++ server/src/controllers/SessionsController.js
@@ -0,0 +1,28 @@
+import UserRepository from "../repositories/UserRepository.js";
+import SessionCreateService from "../services/sessions/SessionCreateService.js";
+
+export class SessionsController{
+    async create(request, response){
+        const { email, password } = request.body;
+
+        const userRepository = new UserRepository();
+        const sessionCreateService = new SessionCreateService(userRepository);
+
+        const { user, token } = await sessionCreateService.execute({email, password});
+
+        response.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+            maxAge: 15 * 60 * 1000
+        });
+    
+        const responseUser = {
+            name: user.name,
+            email: user.email,
+            isAdmin: user.role === "admin"
+        };
+    
+        return response.status(201).json({ user: responseUser });        
+    }
+}

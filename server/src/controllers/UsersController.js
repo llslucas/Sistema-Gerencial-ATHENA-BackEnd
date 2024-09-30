25fb6b06c5aac4e014ca0209b748483fc2ac309f
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-09-29 23:30:15 -0300

    Controller de Usuários criado

---
+++ server/src/controllers/UsersController.js
@@ -0,0 +1,44 @@
+import UserRepository from '../repositories/UserRepository.js';
+import UserCreateService from '../services/users/UserCreateService.js';
+import UserUpdateService from '../services/users/UserUpdateService.js';
+import AppError from '../utils/AppError.js';
+
+export default class UsersController {
+    async create(request, response){
+        const { name, email, password } = request.body;      
+
+        const userRepository = new UserRepository();
+
+        const userCreateService = new UserCreateService(userRepository);
+        await userCreateService.execute({ name, email, password });
+        
+        return response.status(201).json();        
+    }
+
+    async update(request, response){
+        const { name, email, password, old_password } = request.body;
+        const user_id = request.user.id;
+        
+        const userRepository = new UserRepository();
+
+        const userUpdateService = new UserUpdateService(userRepository);
+        
+        await userUpdateService.execute({ user_id, name, email, password, old_password })
+
+        return response.json("Usuário atualizado com sucesso!");
+    }
+
+    async validate(request, response){
+        const id = request.user.id;
+
+        const userRepository = new UserRepository();
+
+        const checkUserExists = await userRepository.findById(id);
+
+        if(!checkUserExists){
+            throw new AppError("JWT token não informado", 401);
+        }
+
+        return response.status(200).json(true);
+    }
+}

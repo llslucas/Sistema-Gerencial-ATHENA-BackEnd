c0deac3146b384571519704d9b28b8274a6bd134
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-10-26 19:36:47 -0300

    Localhost adicionado ao CORS

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'https://athena-front.vercel.app',
+    origin: ['https://athena-front.vercel.app', "http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true
 }));
 
